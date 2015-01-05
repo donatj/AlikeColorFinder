bc1c9220907d7ca1656402977b1f82ea9e71423b
Jesse Donat <donatj@gmail.com>
2015-01-05 16:05:40 -0600

    Further seperation into factory.

--- src/AlikeColorFinder.php
+++ src/AlikeColorFinder.php
@@ -4,8 +4,14 @@ namespace donatj\AlikeColorFinder;
 
 class AlikeColorFinder {
 
+	/**
+	 * @var string
+	 */
 	protected $subject;
 
+	/**
+	 * @var \donatj\AlikeColorFinder\ColorEntryFactory
+	 */
 	protected $factory;
 
 	public function __construct( $subject = "", ColorEntryFactory $colorEntryFactory = null ) {
@@ -153,86 +159,6 @@ class AlikeColorFinder {
 		}
 
 		return $colors;
-	}
-
-	private function rgb2hsl( array $rgb ) {
-		$r = $rgb['r'];
-		$g = $rgb['g'];
-		$b = $rgb['b'];
-
-		$r /= 255;
-		$g /= 255;
-		$b /= 255;
-
-		$h   = 0;
-		$max = max($r, $g, $b);
-		$min = min($r, $g, $b);
-
-		$l = ($max + $min) / 2;
-		$d = $max - $min;
-
-		if( $d == 0 ) {
-			$h = $s = 0; // achromatic
-		} else {
-			$s = $d / (1 - abs(2 * $l - 1));
-
-			switch( $max ) {
-				case $r:
-					$h = 60 * fmod((($g - $b) / $d), 6);
-					if( $b > $g ) {
-						$h += 360;
-					}
-					break;
-
-				case $g:
-					$h = 60 * (($b - $r) / $d + 2);
-					break;
-
-				case $b:
-					$h = 60 * (($r - $g) / $d + 4);
-					break;
-			}
-		}
-
-		return array( 'h' => $h, 's' => $s, 'l' => $l );
-	}
-
-	private function hsla2rgba( $h, $s, $l, $a ) {
-		$c = (1 - abs(2 * $l - 1)) * $s;
-		$x = $c * (1 - abs(fmod(($h / 60), 2) - 1));
-		$m = $l - ($c / 2);
-
-		if( $h < 60 ) {
-			$r = $c;
-			$g = $x;
-			$b = 0;
-		} else if( $h < 120 ) {
-			$r = $x;
-			$g = $c;
-			$b = 0;
-		} else if( $h < 180 ) {
-			$r = 0;
-			$g = $c;
-			$b = $x;
-		} else if( $h < 240 ) {
-			$r = 0;
-			$g = $x;
-			$b = $c;
-		} else if( $h < 300 ) {
-			$r = $x;
-			$g = 0;
-			$b = $c;
-		} else {
-			$r = $c;
-			$g = 0;
-			$b = $x;
-		}
-
-		$r = ($r + $m) * 255;
-		$g = ($g + $m) * 255;
-		$b = ($b + $m) * 255;
-
-		return [ 'r' => $r, 'g' => $g, 'b' => $b, 'a' => $a ];
 	}
 
 }
--- src/ColorEntryFactory.php
+++ src/ColorEntryFactory.php
@@ -9,7 +9,7 @@ class ColorEntryFactory {
 	}
 
 	public function makeFromRgb( $r, $g, $b ) {
-		return new ColorEntry($r, $g, $b);
+		return $this->makeFromRgba($r, $g, $b, 1);
 	}
 
 	public function makeFromHexString( $hex ) {
@@ -24,10 +24,52 @@ class ColorEntryFactory {
 			$g = hexdec(substr($hex, 2, 2));
 			$b = hexdec(substr($hex, 4, 2));
 		} else {
-			throw new \InvalidArgumentException('Invalid Hex "'. $hex .'"');
+			throw new \InvalidArgumentException('Invalid Hex "' . $hex . '"');
 		}
 
 		return new ColorEntry($r, $g, $b, 1);
+	}
+
+	public function makeFromHsla( $h, $s, $l, $a ) {
+		$c = (1 - abs(2 * $l - 1)) * $s;
+		$x = $c * (1 - abs(fmod(($h / 60), 2) - 1));
+		$m = $l - ($c / 2);
+
+		if( $h < 60 ) {
+			$r = $c;
+			$g = $x;
+			$b = 0;
+		} elseif( $h < 120 ) {
+			$r = $x;
+			$g = $c;
+			$b = 0;
+		} elseif( $h < 180 ) {
+			$r = 0;
+			$g = $c;
+			$b = $x;
+		} elseif( $h < 240 ) {
+			$r = 0;
+			$g = $x;
+			$b = $c;
+		} elseif( $h < 300 ) {
+			$r = $x;
+			$g = 0;
+			$b = $c;
+		} else {
+			$r = $c;
+			$g = 0;
+			$b = $x;
+		}
+
+		$r = ($r + $m) * 255;
+		$g = ($g + $m) * 255;
+		$b = ($b + $m) * 255;
+
+		return [ 'r' => $r, 'g' => $g, 'b' => $b, 'a' => $a ];
+	}
+
+	public function makeFromHsl( $h, $s, $l ) {
+		return $this->makeFromHsla($h, $s, $l, 1);
 	}
 
 }
