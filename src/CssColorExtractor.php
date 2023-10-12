091c6dba1dbb8ce94b2c17744011b7cfacd701ce
Jesse Donat <donatj@gmail.com>
2023-10-11 23:15:07 -0500

    Fix grey

--- src/CssColorExtractor.php
+++ src/CssColorExtractor.php
@@ -66,6 +66,7 @@ class CssColorExtractor {
 		'gold'                 => 'ffd700',
 		'goldenrod'            => 'daa520',
 		'gray'                 => '808080',
+		'grey'                 => '808080',
 		'green'                => '008000',
 		'greenyellow'          => 'adff2f',
 		'honeydew'             => 'f0fff0',
